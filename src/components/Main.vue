e40216a15fd806dbcbb28312c0568440c49e180e
Bice93 <bianca.cerulo@hotmail.it>
2022-07-25 17:40:49 +0200

    Added the Condition of the Not search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,29 @@
 <template>
   <main>
     <div class="container">
-      
+      <section v-if="films.length">
         <h1 class="my-4">Film</h1>
         <section class="box_cards d-flex flex-wrap mt-3">
           <CardElement v-for="film in films" 
           :key="film.id" 
           :film="film" />
         </section>
-      
-      
+      </section>
+
+      <section v-if="series.length">
         <h1 class="my-4">Serie Tv</h1>
         <section class="box_cards d-flex flex-wrap mt-3">
-            <CardElementMovie
+          <CardElementMovie
             v-for="movie in series"
             :key="movie.id"
             :movie="movie"
-            />
+          />
         </section>
-      
+      </section>
+
+      <h3 class="my-4" v-if="films.length === 0 && series.length === 0"> 
+        Nessun elemento cercato!
+      </h3>
     </div>
   </main>
 </template>
@@ -52,5 +57,4 @@ main {
   background-color: rgb(105, 105, 105);
   overflow: scroll;
 }
-
 </style>
